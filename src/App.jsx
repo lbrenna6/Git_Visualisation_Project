e71fd1add383c8a8228ef63b733db7f58fc1227b
laura <lbrenna6@tcd.ie>
2019-11-11 23:28:16 +0000

    added more info on public branches, languages user uses and keywords.

--- src/App.jsx
+++ src/App.jsx
@@ -2,51 +2,100 @@ import React, { Component } from 'react';
 import axios from 'axios';import Form from './components/Form.jsx';
 import SortedList from './components/SortedList.jsx';
 import ProfileDetails from './components/ProfileDetails.jsx';
+import LanguageList from './components/LanguageList.jsx';import lda from './lda';
 class App extends Component {
-  
-  constructor() 
-  {
+  constructor() {
     super();
     this.state = {
       gitun: 'No username',
       infoclean : '',
+      info: '',
       formData: {
         username: '',
       },
       repitems: null,
-      staritems: null,}
+      staritems: null,
+      replanguagecount: {},
+      keywords: null
+      }
     this.handleUserFormSubmit = this.handleUserFormSubmit.bind(this);
     this.handleFormChange= this.handleFormChange.bind(this);
   }
 
-  handleUserFormSubmit(event) 
-  {
+  handleUserFormSubmit(event) {
     event.preventDefault();
     axios.get('https://api.github.com/users/'+this.state.formData.username)
     .then(response => this.setState({
       gitun: response.data.login,
       infoclean: response.data,
+      info : JSON.stringify(response.data, undefined, 2)
     })).catch((err) => { console.log(err); });axios.get('https://api.github.com/users/'+this.state.formData.username+'/repos')
-    .then(response => this.setState({
-      repitems : response.data
-      .filter(({fork}) => fork === false)
-      .sort((b, a) => (a.watchers_count + a.forks_count) - (b.watchers_count + b.forks_count)).slice(0,10)
-      })).catch((err) => { console.log(err); });axios.get('https://api.github.com/users/'+this.state.formData.username+'/starred')
-    .then(response => this.setState({
-      staritems : response.data
-      .filter(({fork}) => fork === false)
-      .sort((b, a) => (a.watchers_count + a.forks_count) - (b.watchers_count + b.forks_count)).slice(0,10)
-    })).catch((err) => { console.log(err); });
-  };
+    .then(response => {var itemsWithFalseForks = response.data.filter(item => item.fork === false);
+      var sortedItems = itemsWithFalseForks.sort((b,a) =>
+       {
+        if((a.watchers_count +  a.forks_count) < (b.forks_count + b.watchers_count))
+        {
+          return -1
+        }
+        else if ((a.watchers_count +  a.forks_count) > (b.forks_count + b.watchers_count))
+        {
+          return 1
+        }
+        else 
+        {
+          return 0
+        }
+      })
+      let dictrlc = Object.assign({}, this.state.replanguagecount);
+      for (var i = 0; i < itemsWithFalseForks.length; i++) 
+      {
+          dictrlc[itemsWithFalseForks[i]['language']] = -~ dictrlc[itemsWithFalseForks[i]['language']]
+      }
+      this.setState
+      ({
+        repitems: sortedItems.slice(0,10),
+        replanguagecount: dictrlc,
+      })})
+    .catch((err) => { console.log(err); })
+     axios.get('https://api.github.com/users/'+this.state.formData.username+'/starred')
+    .then(response => {var itemsWithFalseForks = response.data.filter(item => item.fork === false);
+      var sortedItems = itemsWithFalseForks.sort((b,a) => 
+      {
+        if((a.watchers_count +  a.forks_count) < (b.forks_count + b.watchers_count)){
+          return -1
+        }else if ((a.watchers_count +  a.forks_count) > (b.forks_count + b.watchers_count)){
+          return 1
+        }else {
+          return 0
+        }
+      });
 
-  handleFormChange(event) {
+      var documents = []
+      for (var i = 0; i < response.data.length; i++) {
+          var descr = response.data[i]['description']
+          if (descr != null) {
+            var newtext = descr.match(/[^.!?]+[.!?]+/g)
+            if (newtext != null) {
+              documents = documents.concat(newtext)
+            }
+          }
+      }
+      var result = lda(documents, 3, 3);
+      var keywords = new Set()
+      for (var k = 0; k < 3; k++) {
+        for (var j = 0; j < 3; j++) {
+          keywords = keywords.add(result[k][j]['term']);
+        }
+      }this.setState({
+        staritems: sortedItems.slice(0,10),
+        keywords: Array.from(keywords).join(', ')
+      })}).catch((err) => { console.log(err); })};handleFormChange(event) {
     const obj = this.state.formData;
     obj[event.target.name] = event.target.value;
     this.setState(obj);
   };
 
-  render()
-  {
+  render() {
     return (
       <div className="App">
         <header className="App-header">
@@ -69,7 +118,12 @@ class App extends Component {
         <SortedList repitems={this.state.repitems}/>
         <hr></hr>
         Starred Repositories:
-        <SortedList repitems={this.state.staritems}/></div>
+        <SortedList repitems={this.state.staritems}/>
+        <hr></hr>
+        Own Repos Language Count:
+        <LanguageList langslist={this.state.replanguagecount}/>
+         Keywords:  {this.state.keywords}
+      </div>
     );
   }
-}export default App;
+} export default App;
